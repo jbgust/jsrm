c9c2aef8b77fd82a5da3515d9a3b1da7f6689696
jbgust <jeromebise@hotmail.com>
2018-12-19 13:41:46 +0100

    Adding propellant : *  KNSU *  KNSB_FINE *  KNSB_COARSE *  KNER_COARSE *  KNMN_COARSE

--- src/main/java/com/jsrm/motor/propellant/PropellantType.java
+++ src/main/java/com/jsrm/motor/propellant/PropellantType.java
@@ -7,18 +7,62 @@ import static com.google.common.collect.Range.atLeast;
 import static com.google.common.collect.Range.closedOpen;
 
 public enum PropellantType implements SolidPropellant {
-    KNDX(
-            "KNDX = Potassium Nitrate/Dextrose, 65/35 O/F ratio",
-            1.879, 1.043, 1.131, 42.39, 1710,
+    KNDX("KNDX = Potassium Nitrate/Dextrose, 65/35 O/F ratio",
+            1.879, 1.043, 1.1308, 42.39, 1710,
             new ImmutableRangeMap.Builder<Double, BurnRateData>()
                     .put(closedOpen(0.1, 0.779135), new BurnRateData(8.87544496778536, 0.6193))
                     .put(closedOpen(0.779135, 2.571835), new BurnRateData(7.55278442387944, -0.0087))
                     .put(closedOpen(2.571835, 5.9297), new BurnRateData(3.84087990499602, 0.6882))
                     .put(closedOpen(5.9297, 8.501535), new BurnRateData(17.2041864098062, -0.1481))
                     //According to Richard Nakka burnrate Excel sheet the last range should be 8,501535 to 11,204375
-                    // but the algorithm return also this BurnRateData if the pressure exceed 11,204375 MPa
+                    // but the algorithm return also this BurnRateData even if the pressure exceed 11,204375 MPa
                     .put(atLeast(8.501535), new BurnRateData(4.77524086347659, 0.4417))
-                    .build());
+                    .build()),
+
+    KNSB_FINE("KNSB fine = potassium nitrate/sorbitol 65/35 O/F ratio, oxidizer finely milled",
+            1.841, 1.042, 1.1361, 39.90, 1600,
+            new ImmutableRangeMap.Builder<Double, BurnRateData>()
+                    .put(closedOpen(0.101, 0.806715), new BurnRateData(10.7076837980331, 0.6247))
+                    .put(closedOpen(0.806715, 1.50311), new BurnRateData(8.76328007101773, -0.3142))
+                    .put(closedOpen(1.50311, 3.79225), new BurnRateData(7.85216579497841, -0.013))
+                    .put(closedOpen(3.79225, 7.0329), new BurnRateData(3.90676830413905, 0.5354))
+                    //According to Richard Nakka burnrate Excel sheet the last range should be 7.0329 to 10,67346
+                    // but the algorithm return also this BurnRateData even if the pressure exceed 10,67346 MPa
+                    .put(atLeast(7.0329), new BurnRateData(9.65320361987685, 0.0638))
+                    .build()),
+
+    KNSB_COARSE("KNSB coarse = potassium nitrate/sorbitol 65/35 O/F ratio, oxidizer granular or lightly milled prills",
+            1.841, 1.042, 1.1361, 39.90, 1600,
+            new ImmutableRangeMap.Builder<Double, BurnRateData>()
+                    //According to Richard Nakka burnrate Excel sheet the last range should be 0.101 to 10,3
+                    // but the algorithm return also this BurnRateData even if the pressure exceed 10,3 MPa
+                    .put(atLeast(0.101), new BurnRateData(5.13, 0.22))
+                    .build()),
+
+    KNSU("KNSU = potassium nitrate/sucrose 65/35 O/F ratio, oxidizer finely milled",
+            1.889, 1.044, 1.133, 41.98, 1720,
+            new ImmutableRangeMap.Builder<Double, BurnRateData>()
+                    //According to Richard Nakka burnrate Excel sheet the last range should be 0.101 to 10,3
+                    // but the algorithm return also this BurnRateData even if the pressure exceed 10,3 MPa
+                    .put(atLeast(0.101), new BurnRateData(8.26, 0.319))
+                    .build()),
+
+    KNER_COARSE("KNER coarse = potassium nitrate/erythritol 65/35 O/F ratio, oxidizer granular or lightly milled prills",
+            1.820, 1.0426, 1.139, 38.78, 1608,
+            new ImmutableRangeMap.Builder<Double, BurnRateData>()
+                    //According to Richard Nakka burnrate Excel sheet the last range should be 0.101 to 10,3
+                    // but the algorithm return also this BurnRateData even if the pressure exceed 10,3 MPa
+                    .put(atLeast(0.101), new BurnRateData(2.9, 0.4))
+                    .build()),
+
+    KNMN_COARSE("KNMN coarse = potassium nitrate/mannitol 65/35 O/F ratio, oxidizer granular or lightly milled prills",
+            1.854, 1.042, 1.1363, 39.826, 1616,
+            new ImmutableRangeMap.Builder<Double, BurnRateData>()
+                    //According to Richard Nakka burnrate Excel sheet the last range should be 0.101 to 10,3
+                    // but the algorithm return also this BurnRateData even if the pressure exceed 10,3 MPa
+                    .put(atLeast(0.101), new BurnRateData(5.13, 0.22))
+                    .build()
+    );
 
     private final RangeMap<Double, BurnRateData> byPressureData;
     private final String description;
--- src/test/java/com/jsrm/motor/propellant/PropellantTypeTest.java
+++ src/test/java/com/jsrm/motor/propellant/PropellantTypeTest.java
@@ -1,24 +1,143 @@
 package com.jsrm.motor.propellant;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.EnumSource;
 
-import static com.jsrm.motor.propellant.PropellantType.KNDX;
+import static com.jsrm.motor.propellant.PropellantType.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PropellantTypeTest {
 
-    @Test
-    void shouldThrowExceptionIfKNDXBurnRateCoefficientIsUsedWithOutOfBoundChamberPressure() {
-        assertThatThrownBy(() -> KNDX.getBurnRateCoefficient(0.09))
-                .isInstanceOf(ChamberPressureOutOfBoundException.class)
-                .hasMessage("KNDX has no burn rate coefficient for this pressure (0.09) should be in range [0.1..+∞)");
+    private static final double DELTA_PRESSURE = 0.00001d;
+
+    private static int KNDX_lineNumber;
+    private static int KNSB_FINE_lineNumber;
+
+    @BeforeAll
+    static void setUp() {
+        KNDX_lineNumber = 0;
+        KNSB_FINE_lineNumber = 0;
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/propellant/propellant_data.csv", numLinesToSkip = 1, delimiter = '|')
+    void checkPropellantdata(String propellantName, double idealGrainMassDensity, double k2Ph, double k, double effectiveMolecularWeight, double chamberTemperature) {
+        SolidPropellant propellant = PropellantType.valueOf(propellantName);
+
+        assertThat(propellant.getIdealMassDensity()).isEqualTo(idealGrainMassDensity);
+        assertThat(propellant.getK2Ph()).isEqualTo(k2Ph);
+        assertThat(propellant.getK()).isEqualTo(k);
+        assertThat(propellant.getEffectiveMolecularWeight()).isEqualTo(effectiveMolecularWeight);
+        assertThat(propellant.getChamberTemperature()).isEqualTo(chamberTemperature);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/propellant/KNDX_burnrate.csv", numLinesToSkip = 1, delimiter = '|')
+    void checkKNDXBurnRateData(double minPressureInChamber, double maxPressureInChamber, double burnRateCoefficient, double pressureExponent) {
+        assertPropellantBurnRateCoefficient(minPressureInChamber, maxPressureInChamber, burnRateCoefficient, KNDX, KNDX_lineNumber, 5);
+        assertPropellantPressureExponent(minPressureInChamber, maxPressureInChamber, pressureExponent, KNDX, KNDX_lineNumber++, 5);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/propellant/KNSB_FINE_burnrate.csv", numLinesToSkip = 1, delimiter = '|')
+    void checkKNSB_FINEBurnRateData(double minPressureInChamber, double maxPressureInChamber, double burnRateCoefficient, double pressureExponent) {
+        assertPropellantBurnRateCoefficient(minPressureInChamber, maxPressureInChamber, burnRateCoefficient, KNSB_FINE, KNSB_FINE_lineNumber, 5);
+        assertPropellantPressureExponent(minPressureInChamber, maxPressureInChamber, pressureExponent, KNSB_FINE, KNSB_FINE_lineNumber++, 5);
     }
 
     @Test
-    void shouldThrowExceptionIfKNDXPressureExponentIsUsedWithOutOfBoundChamberPressure() {
-        assertThatThrownBy(() -> KNDX.getPressureExponent(0.09))
-                .isInstanceOf(ChamberPressureOutOfBoundException.class)
-                .hasMessage("KNDX has no pressure exponent for this pressure (0.09) should be in range [0.1..+∞)");
+    void checkKNSB_COARSEBurnRateData() {
+        double minPressureInChamber = 0.101;
+        double maxPressureInChamber = 10.3;
+        assertPropellantBurnRateCoefficient(minPressureInChamber, maxPressureInChamber, 5.13, KNSB_COARSE);
+        assertPropellantPressureExponent(minPressureInChamber, maxPressureInChamber, 0.22, KNSB_COARSE);
     }
 
+    @Test
+    void checkKNMN_COARSEBurnRateData() {
+        double minPressureInChamber = 0.101;
+        double maxPressureInChamber = 10.3;
+        assertPropellantBurnRateCoefficient(minPressureInChamber, maxPressureInChamber, 5.13, KNMN_COARSE);
+        assertPropellantPressureExponent(minPressureInChamber, maxPressureInChamber, 0.22, KNMN_COARSE);
+    }
+
+    @Test
+    void checkKNSUBurnRateData() {
+        double minPressureInChamber = 0.101;
+        double maxPressureInChamber = 10.3;
+        assertPropellantBurnRateCoefficient(minPressureInChamber, maxPressureInChamber, 8.26, KNSU);
+        assertPropellantPressureExponent(minPressureInChamber, maxPressureInChamber, 0.319, KNSU);
+    }
+
+    @Test
+    void checkKNER_COARSEBurnRateData() {
+        double minPressureInChamber = 0.101;
+        double maxPressureInChamber = 10.3;
+        assertPropellantBurnRateCoefficient(minPressureInChamber, maxPressureInChamber, 2.9, KNER_COARSE);
+        assertPropellantPressureExponent(minPressureInChamber, maxPressureInChamber, 0.4, KNER_COARSE);
+    }
+
+    @ParameterizedTest
+    @EnumSource(PropellantType.class)
+    void shouldThrowExceptionIfKNDXBurnRateCoefficientIsUsedWithOutOfBoundChamberPressure(PropellantType propellantType) {
+        assertThatThrownBy(() -> propellantType.getBurnRateCoefficient(0.09))
+                .isInstanceOf(ChamberPressureOutOfBoundException.class)
+                .hasMessageContaining(propellantType + " has no burn rate coefficient for this pressure (0.09) should be in range");
+    }
+
+    @ParameterizedTest
+    @EnumSource(PropellantType.class)
+    void shouldThrowExceptionIfKNDXPressureExponentIsUsedWithOutOfBoundChamberPressure(PropellantType propellantType) {
+        assertThatThrownBy(() -> propellantType.getPressureExponent(0.09))
+                .isInstanceOf(ChamberPressureOutOfBoundException.class)
+                .hasMessageContaining(propellantType + " has no pressure exponent for this pressure (0.09) should be in range");
+    }
+
+
+    private void assertPropellantBurnRateCoefficient(double minPressureInChamber, double maxPressureInChamber, double burnRateCoefficient, PropellantType propellantType) {
+        assertPropellantBurnRateCoefficient(minPressureInChamber, maxPressureInChamber, burnRateCoefficient, propellantType, 0, 1);
+    }
+
+    private void assertPropellantBurnRateCoefficient(double minPressureInChamber, double maxPressureInChamber, double burnRateCoefficient, PropellantType propellantType, int lineNumber, int numberOfLine) {
+        assertThat(propellantType.getBurnRateCoefficient(minPressureInChamber)).isEqualTo(burnRateCoefficient);
+        assertThat(propellantType.getBurnRateCoefficient(maxPressureInChamber - DELTA_PRESSURE)).isEqualTo(burnRateCoefficient);
+
+        if (lineNumber == 0 && numberOfLine > 1) {
+            assertThat(propellantType.getBurnRateCoefficient(maxPressureInChamber)).isNotEqualTo(burnRateCoefficient);
+        } else if (numberOfLine - 1 == lineNumber) {
+            if (numberOfLine > 1) {
+                assertThat(propellantType.getBurnRateCoefficient(minPressureInChamber - DELTA_PRESSURE)).isNotEqualTo(burnRateCoefficient);
+            }
+            assertThat(propellantType.getBurnRateCoefficient(maxPressureInChamber)).isEqualTo(burnRateCoefficient);
+        } else {
+            assertThat(propellantType.getBurnRateCoefficient(minPressureInChamber - DELTA_PRESSURE)).isNotEqualTo(burnRateCoefficient);
+            assertThat(propellantType.getBurnRateCoefficient(maxPressureInChamber)).isNotEqualTo(burnRateCoefficient);
+        }
+
+    }
+
+    private void assertPropellantPressureExponent(double minPressureInChamber, double maxPressureInChamber, double burnRateCoefficient, PropellantType propellantType) {
+        assertPropellantPressureExponent(minPressureInChamber, maxPressureInChamber, burnRateCoefficient, propellantType, 0, 1);
+    }
+
+    private void assertPropellantPressureExponent(double minPressureInChamber, double maxPressureInChamber, double pressureExponent, PropellantType propellantType, int lineNumber, int numberOfLine) {
+        assertThat(propellantType.getPressureExponent(minPressureInChamber)).isEqualTo(pressureExponent);
+        assertThat(propellantType.getPressureExponent(maxPressureInChamber - DELTA_PRESSURE)).isEqualTo(pressureExponent);
+
+        if (lineNumber == 0 && numberOfLine > 1) {
+            assertThat(propellantType.getPressureExponent(maxPressureInChamber)).isNotEqualTo(pressureExponent);
+        } else if (numberOfLine - 1 == lineNumber) {
+            if (numberOfLine > 1) {
+                assertThat(propellantType.getPressureExponent(minPressureInChamber - DELTA_PRESSURE)).isNotEqualTo(pressureExponent);
+            }
+            assertThat(propellantType.getPressureExponent(maxPressureInChamber)).isEqualTo(pressureExponent);
+        } else {
+            assertThat(propellantType.getPressureExponent(minPressureInChamber - DELTA_PRESSURE)).isNotEqualTo(pressureExponent);
+            assertThat(propellantType.getPressureExponent(maxPressureInChamber)).isNotEqualTo(pressureExponent);
+        }
+    }
 }
